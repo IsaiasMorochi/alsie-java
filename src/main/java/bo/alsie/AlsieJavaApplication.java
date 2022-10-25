7f04d015793b0134c7cb42382c15928539a447be
IsaiasMorochi <imorochi.dev@gmail.com>
2022-10-24 21:17:28 -0400

    first commit

---
+++ src/main/java/bo/alsie/AlsieJavaApplication.java
@@ -0,0 +1,13 @@
+package bo.alsie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsieJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlsieJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bo/alsie/AlsieJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.alsie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsieJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
